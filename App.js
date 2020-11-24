faf5c5cd3dc9e1fe9d55c2bfd8fefe5fea3656eb
gomaaygo <maiurygarcia@gmail.com>
2020-11-24 11:45:34 -0300

    Add style.

--- App.js
+++ App.js
@@ -1,114 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+import styled from 'styled-components';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const Pagina = styled.View`
+  flex: 1;
+`;
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Cabecalho = styled.View`
+  height: 65px;
+  background-color: #fff;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-const App: () => React$Node = () => {
+const Menu = styled.View`
+  height: 50px;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 15px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-color: #24292e;
+  border-width: 1px;
+  border-radius: 5px
+`;
+
+const Logo = styled.Image`
+  width: 50px;
+  height: 50px;
+  margin-left: 15px;
+`;
+
+const Busca = styled.TextInput`
+  color: #24292e;
+  font-size: 25px;
+  margin-left: 20px;
+`;
+
+const Botao = styled.TouchableOpacity``;
+const Iconebuscar = styled.Image`
+  width: 35px;
+  height: 35px;
+  margin-right: 30px;
+`;
+
+const Imagem = styled.Image`
+  margin-left: 100px;
+  margin-right: 100px;
+  margin-bottom: 200px;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+`;
+
+const Avatar = styled.SafeAreaView`
+  width: 200px;
+  height: 300px;
+  margin-bottom: 30px;
+`;
+
+const Title = styled.Text`
+  font-size: 25px;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+  text-align: center;
+`;
+
+const Details = styled.SafeAreaView`
+`;
+
+const Item = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  margin-left: 20px;
+  margin-right: 20px;
+  text-align: center;
+`;
+
+const ItemB = styled.Text`
+  font-size: 15px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-top: 2px;
+  text-align: center;
+`;
+
+const App = () => {
+  const [nome, alteraNome] = useState('');
+  const [filme, alteraFilme] = useState({});
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    <Pagina>
+      <Cabecalho>
+        <Logo source={require('./src/imagens/logo.png')}/>
+      </Cabecalho>
+      <Menu>
+        <Busca placeholder="Busca" placeholderTextColor="#24292e" onChangeText={(username) => alteraNome(username)}/>
+        <Botao activeOpacity={0.3}>
+            <Iconebuscar source={require('./src/imagens/icon-search.png')}/>
+        </Botao>
+      </Menu>
+      <Avatar>
+        <Imagem/>
+      </Avatar>
+      <Details>
+        <Item>Nome</Item>
+        <ItemB>Exemplo</ItemB>
+        <Item>Username</Item>
+        <ItemB></ItemB>
+        <Item>Seguidores</Item>
+        <ItemB></ItemB>
+      </Details>
+    </Pagina>
+  )
+}
 
 export default App;
